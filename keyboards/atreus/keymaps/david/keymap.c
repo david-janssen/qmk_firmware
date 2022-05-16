311b5d0f57dccaebc185f17093338b16351ada81
David Janssen <janssen.dhj@gmail.com>
2022-05-16 18:58:15 +0200

    Small fixes

--- keyboards/atreus/keymaps/david/keymap.c
+++ keyboards/atreus/keymaps/david/keymap.c
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,                      KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,                      KC_G,    KC_H,    KC_J,    KC_K,    KC_L,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,                      KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,
-    KC_TAB,  KC_LALT, KC_M,    KC_ENT,  KC_SPC,  KC_NO,   KC_NO,   KC_LEFT, KC_RGHT, KC_SLSH, TD(OUT), KC_P
+    KC_TAB,  KC_LALT, KC_M,    KC_ENT,  KC_SPC,  KC_NO,   KC_NO,   KC_LEFT, KC_RGHT, KC_MINS, TD(OUT), KC_P
   ),
 
   // KMonad compat, no functionality except 1 tap-dance to escape
@@ -76,6 +76,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
     KC_A,    KC_R,    KC_S,    KC_T,    KC_D,                      KC_H,    KC_N,    KC_E,    KC_I,    KC_O,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-    KC_TAB,  KC_F4,   KC_F5,   KC_LSFT, KC_LCTL, KC_F6,   KC_ESC,  KC_SPC,  KC_LGUI, KC_MINS, TD(OUT), KC_ENT
+    KC_TAB,  KC_F4,   KC_F3,   KC_LSFT, KC_LCTL, KC_F6,   KC_ESC,  KC_SPC,  KC_LGUI, KC_MINS, TD(OUT), KC_ENT
   )
 };
