2ed5b4f86fd192b757140373dbf950fb784dab73
David Janssen <janssen.dhj@gmail.com>
2022-12-14 08:37:34 +0100

    Reopening two keys

--- keyboards/atreus/keymaps/david/keymap.c
+++ keyboards/atreus/keymaps/david/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
     KC_A,    KC_R,    KC_S,    KC_T,    KC_D,                      KC_H,    KC_N,    KC_E,    KC_I,    KC_O,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_LALT,
-    KC_TAB,  KC_NO,   KC_NO,   KC_LSFT, KC_LCTL, L_NUM,   KC_ESC,  KC_SPC,  KC_LGUI, KC_MINS, KC_SLSH, KC_ENT
+    KC_TAB,  KC_F1,   KC_F2,   KC_LSFT, KC_LCTL, L_NUM,   KC_ESC,  KC_SPC,  KC_LGUI, KC_MINS, KC_SLSH, KC_ENT
   ),
 
   // Basic numbers and symbols
